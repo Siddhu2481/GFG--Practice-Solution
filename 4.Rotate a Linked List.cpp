442b6dda6b80f9a4fc3caff2189781337a9df6bb
Siddharth Sharma <105628623+Siddhu2481@users.noreply.github.com>
2022-10-02 21:19:38 +0530

    Create 4.Rotate a Linked List.cpp

---
+++ 4.Rotate a Linked List.cpp
@@ -0,0 +1,18 @@
+if(head==NULL || head->next==NULL || k==0)
+        {
+            return head;
+        }
+        Node* temp=head;
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=head;
+        while(k--)
+        {
+            temp=temp->next;
+            
+        }
+        head=temp->next;
+        temp->next=NULL;
+        return head;
