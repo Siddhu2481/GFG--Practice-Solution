04d766704e6a9477e1f8f507b49c954c30e75469
Siddharth Sharma <105628623+Siddhu2481@users.noreply.github.com>
2022-10-20 22:48:41 +0530

    Create 19.Binary Search.cpp

---
+++ 19.Binary Search.cpp
@@ -0,0 +1,16 @@
+int l=0;
+        int h=n-1;
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            if(arr[mid]==k)
+            return mid;
+            else if(k>arr[mid])
+            {l=mid+1;}
+            else
+            {
+            h=mid-1;
+            }
+            
+        }
+        return -1;
