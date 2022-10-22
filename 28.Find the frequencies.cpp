45fec2b6dc88c32d1a6505d8062913287c253385
Siddharth Sharma <105628623+Siddhu2481@users.noreply.github.com>
2022-10-22 22:11:11 +0530

    Create 28.Find the frequencies.cpp

---
+++ 28.Find the frequencies.cpp
@@ -0,0 +1,13 @@
+unordered_map<int,int> mp;
+    for(auto it:v)
+    {
+        if(it==x)
+        {
+            mp[x]++;
+        }
+    }
+    if(mp.size()!=0)
+    {
+        return mp[x];
+    }
+    return 0;
